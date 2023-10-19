a0e4a6b7171c2b3da81e169b38c9f79c76188a78
Rubens <rubens_olv@hotmail.com>
2023-10-18 21:11:32 -0300

    Including new parallel material.

---
+++ DownloadVideos_and_Segmentation/yt_download_from_csv_paralel.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+import re
+from difflib import SequenceMatcher
+from thefuzz import fuzz
+
+from pytube import YouTube
+import csv
+from multiprocessing import Pool
+
+def download_video(row):
+    name_file = 'nesmdb_mp4/'+row[0]+'.mp4'      
+    streams = YouTube(row[2]).streams.get_by_resolution('240p')
+    if streams is not None:
+        streams.download(filename=name_file, max_retries=3)                    
+    else:
+        YouTube(row[2]).streams.get_lowest_resolution().download(filename=name_file, max_retries=3)
+        
+if __name__ == '__main__':    
+    
+    rows = list()
+    with open('nesmdb_csv_youtube.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:                           
+                rows.append(row)
+                line_count += 1            
+    print(f'Processed {line_count} lines.')                    
+    pool = Pool(processes=5)
+    pool.map(download_video,rows)
+    pool.close()    
+        
